24ece0a825e10e5a938866c0996f20105e1b707a
Hrishikesh Suresh <blackleghubot@github.com>
2018-10-26 10:39:15 +0530

    Need to get key value pairs

--- src/hashtag_extraction.py
+++ src/hashtag_extraction.py
@@ -1,6 +1,8 @@
 import tweepy
 import csv
 import pandas as pd
+import json
+import pprint
 
 #input your credentials here
 
@@ -14,12 +16,19 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
 # Open/Create a file to append data
-csvFile = open('tweet_data.csv', 'w')
+#csvFile = open('tweet_dataX.csv', 'w')
+f = open('tweet_dataX.txt', 'w')
+f_text = open('tweet_text.txt', 'w')
+pp = pprint.PrettyPrinter(indent = 2)
 #Use csv Writer
-csvWriter = csv.writer(csvFile)
+#csvWriter = csv.writer(csvFile)
 
 for tweet in tweepy.Cursor(api.search,q="#SkelligeTsunamiNotReal",
                            lang="en",
                            since="2018-10-03").items():
-    print (tweet.created_at, tweet.text)
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    pp.pprint(tweet._json)
+	#csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    #csvWriter.writerow(str(tweet))
+    f.write(str(tweet._json))
+    f_text.write(str(tweet.text))
+f.close()
