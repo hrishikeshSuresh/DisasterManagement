25160da2a059b56b43ad8ec412fd79010e8fe10f
redlegblackarm <hrishikesh123s@gmail.com>
2018-10-26 15:00:57 +0530

    Version 1

--- src/connectDatabase.py
+++ src/connectDatabase.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 #importing MongoClient from PyMongo
 from pymongo import MongoClient
 
 #importing pandas and other libraries
 import pandas as pd
 import matplotlib.pyplot as plt
-from pprint import pprint
+import pprint
 import sys
 from pandas.tools.plotting import scatter_matrix
 
@@ -21,39 +23,46 @@ if("SourceDB" in client.database_names()):
 db = client.SourceDB
 
 #connecting to collection(table)
-collection1 = db.shelterInfo
+collection1 = db.rssInfo
+
+pp = pprint.PrettyPrinter(indent = 2)
 
 #selecting all rows in collection
 data_json = collection1.find()
 print(data_json)
 
 #creating sql file which can be run into mongodb directly from bash
-fp = open("tweets.sql", "w")
-fp.write("use SourceDB\n")
-fp.write("show collections\n")
+#fp = open("tweets.sql", "w")
+
+#fp.write("use SourceDB\n")
+#fp.write("show collections\n")
+
 
 #variable to see which lines from input are not recognized
 i = 0
 #for inserting values into collection
-with open("tweets_sample.json") as f:
+with open("tweet_src.txt") as f:
 #for line in sys.stdin:
 	for line in f:
-		pprint(line)
+		pp.pprint(line)
 		try:	
-			#row = collection1.insert_one(line)
-			fp.write("db.shelterInfo.insert("+ line +")\n")
+			row = collection1.insert_one(line)
+			#fp.write("db.shelterInfo.insert("+ line +")\n")
 			#print(row.inserted_id)
 		except:
 			print(i)
 			i = i + 1
 			pass;
 
-fp.close()
+#fp.close()
 #or use sys.stdin if data is too large
 #can be added depending on the size of json file
 
 data_json = collection1.find()
 print(data_json)
+client.close()
+
+exit(0)
 
 #to get description and summary of data
 data = pd.DataFrame(list(collection1.find()))
